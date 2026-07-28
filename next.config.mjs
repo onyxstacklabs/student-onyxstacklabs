564f04ab8463a19a96f533874d53f9d2b12a4fed
OnyxStack Labs <onyxstacklabs@gmail.com>
2026-07-28 16:46:26 +0500

    Remove security headers and related config

--- next.config.mjs
+++ next.config.mjs
@@ -1,47 +1,7 @@
 /** @type {import('next').NextConfig} */
-const securityHeaders = [
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload',
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin',
-  },
-];
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/:(.*)',
-        headers: securityHeaders,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
