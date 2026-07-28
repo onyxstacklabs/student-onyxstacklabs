e14f7976deed6bcc87186e11186ab124b461f2fa
OnyxStack Labs <onyxstacklabs@gmail.com>
2026-07-28 16:50:24 +0500

    Configure ESLint and TypeScript to ignore build errors
    Added ESLint and TypeScript configurations to ignore errors during builds.

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Build ke waqt ESLint errors ki wajah se deployment cancel nahi hone dega
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Typecheck errors ko bhi build ke dauran ignore karega
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
