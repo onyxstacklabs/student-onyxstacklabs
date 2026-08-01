1b220fc7395328a5d5990e600a864a1dae351ce5
OnyxStack Labs <onyxstacklabs@gmail.com>
2026-08-01 10:03:56 +0500

    Add PLATFORM_CONFIG for company details

---
+++ src/lib/config/platform.ts
@@ -0,0 +1,7 @@
+export const PLATFORM_CONFIG = {
+  companyName: 'OnyxStack Labs',
+  supportEmail: 'support@onyxstacklabs.com',
+  infoEmail: 'info@onyxstacklabs.com',
+  whatsappNumber: '+92 344 5800630',
+  whatsappLink: 'https://wa.me/923445800630',
+};
